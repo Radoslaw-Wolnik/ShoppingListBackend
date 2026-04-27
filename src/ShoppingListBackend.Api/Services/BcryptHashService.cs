bcb15dd6b3fce4695de890d12cb0106c1248a2ff
Radoslaw-Wolnik <radek.wolnik@gmail.com>
2026-04-27 14:07:54 +0200

    fixed bcrypt

--- src/ShoppingListBackend.Api/Services/BcryptHashService.cs
+++ src/ShoppingListBackend.Api/Services/BcryptHashService.cs
@@ -1,9 +1,9 @@
-using BCrypt.Net;
+using BC = BCrypt.Net.BCrypt;
 
 namespace ShoppingListBackend.Api.Services;
 
 public class BcryptHashService : IHashService
 {
-    public string Hash(string input) => BCrypt.HashPassword(input);
-    public bool Verify(string input, string hash) => BCrypt.Verify(input, hash);
+    public string Hash(string input) => BC.HashPassword(input);
+    public bool Verify(string input, string hash) => BC.Verify(input, hash);
 }
